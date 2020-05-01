dc583e35ee0043da7197113f7c828459b1ed3301
Stanislas Bernard <bernardstanislas@gmail.com>
2020-05-01 18:30:58 +0200

    :iphone: Hide website on desktop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className="justify-center items-center hidden md:flex h-screen">
+        <h1 className="text-xl">Rendez-vous sur mobile !</h1>
+      </div>
+      <div className="md:hidden">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
